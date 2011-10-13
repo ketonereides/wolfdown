fcee4b9e0f82c4aa091555043667e0069c78001c
Keto Nereides <keto@ontheclock.us>
2011-10-13 01:00:18 -0400

    blah blah blah

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 // this will be the title that shows up in the browser and on the page
 $siteTitle = "Wolfdown";
 
-// should be list items (<li><a href="#">Link</a></li>); remember to escape quotes with a backslash!
+// should be list items (<li><a href=\"#\">Link</a></li>); remember to escape quotes with a backslash!
 $customLinks = "
   <li><a href=\"https://github.com/ketonereides/wolfdown\">Get Wolfdown</a></li>";
 
